695ec8f04b17d149680b3d6e5b765b2758f5cf45
yoontaehoonDev <siryou10@gmail.com>
2021-04-19 17:52:48 +0900

    Server/Client

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -14,6 +14,8 @@ public class ClientApp {
   public static void main(String[] args) {
     ClientApp app = new ClientApp("localhost", 8888);
 
+
+
     try {
       app.execute();
 
@@ -28,48 +30,47 @@ public class ClientApp {
     this.port = port;
   }
 
-
   public void execute() throws Exception {
-
-    try(
+    // Stateful 통신 방식
+    try (
+        // 1) 서버와 연결하기
         Socket socket = new Socket(serverAddress, port);
-        // 2. 데이터 입출력 스트림 객체 준비
+
+        // 2) 데이터 입출력 스트림 객체를 준비
         PrintWriter out = new PrintWriter(socket.getOutputStream());
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));  
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         ) {
-      // Stateful 통신 방식
-      // 1. 서버와 연결
 
       while (true) {
         String command = com.eomcs.util.Prompt.inputString("명령> ");
-
         if (command.length() == 0) {
           continue;
         }
 
+        // 서버에 명령을 보낸 후 그 결과를 받아 출력한다.
         out.println(command);
         out.println();
         out.flush();
 
         String line = null;
-        while(true) {
+        while (true) {
           line = in.readLine();
-          if(line.length() == 0) {
+          if (line.length() == 0) {
             break;
           }
           System.out.println(line);
         }
         System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
 
-        if(command.equalsIgnoreCase("quit") || 
+        if (command.equalsIgnoreCase("quit") || 
             command.equalsIgnoreCase("exit") ||
             command.equalsIgnoreCase("serverstop")) {
           System.out.println("안녕!");
           break;
-        } 
+        }
       }
-    } 
-    catch (Exception e) {
+
+    } catch (Exception e) {
       System.out.println("통신 오류 발생!");
     }
 
